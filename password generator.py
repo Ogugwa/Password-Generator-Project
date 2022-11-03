78f97059e7234dc80a3b2b8c8ca6b6cc4ddb915e
Ogugwa <99969654+Ogugwa@users.noreply.github.com>
2022-11-03 13:41:21 +0100

    Update password generator.py

--- password generator.py
+++ password generator.py
@@ -9,4 +9,4 @@ for i in range(20):
 print( "Your password is : " + password_generator)
 #3.The range signifies the number of time we want the code to select a character from
 # the range of values from 25 to 99
-# The character function makes it easy for the random integer to include Alphabets and Special characters in the code
+# The chr is a module in python just like str and int;it makes it easy for the random integer to include Alphabets and Special characters in the code
